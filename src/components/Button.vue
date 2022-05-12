e15642cca1a2cfab103e9ecd9b9b73800dd3de77
Nick <nick-graus@hotmail.com>
2022-05-12 09:16:36 +0200

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="button">{{buttonName}}</div>
+</template>
+<script>
+export default {
+  name: 'button',
+  data () {
+    return {
+      buttonName: "this is a button"
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
