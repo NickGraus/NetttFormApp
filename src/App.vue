935ec80e53df151a8bb1205c2c1833759d6355b5
Nick <nick-graus@hotmail.com>
2022-05-12 11:48:08 +0200

    created button component

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,41 @@
+
+
+
+
 <template>
-  <div id="app">
-    <Button />
-  </div>
+  <div id="app"></div>
+      <Button btnName="Button" state="secondary"></Button>
+  <Button btnName="Button" state="primary"></Button>
+
 </template>
 
-
-
 <script>
-  import Button from './components/Button'
-  export default {
-    name: 'app',
-    components: {
-      Button
-    }
-  }
+import Button from "./components/BtnComp.vue";
+
+export default {
+  name: "cardComponent",
+  components: {
+    Button,
+  },
+  data() {
+    return {};
+  },
+
+  props: {
+    title: String,
+    description: String,
+  },
+
+  methods: {},
+};
 </script>
 
-
-
 <style>
-@import "./assets/base.css";
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
+@font-face {
+  font-family: "SF Pro Text";
+  src: local("SF Pro Text"), url("./fonts/SFProText-Semibold.ttf") format("truetype");
 }
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
+ #app {
+   font-family: "SF Pro Text";
+ }
 </style>
---
+++ src/components/BtnComp.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="btn" :class="state">{{btnName}}</div>
+</template>
+
+
+
+<script>
+export default {
+  name: 'btnComp',
+  data() {
+    return {
+    };
+  },
+
+  props: {
+    btnName: String,
+    state: String,
+  },
+
+  methods: {}
+};
+</script>
+
+<style>
+ #btn {
+   padding: 16px;
+   display: inline-block;
+   font-size: 14px;
+   text-align: center;
+   border-radius: 4px;
+ }
+ .primary {
+   background-image: linear-gradient(to right, #7D0FB0 , #BF1DC2);
+   color: white;
+ }
+
+ .secondary {
+   background-color: #ffffff;
+   color: #9B15B8;
+   box-sizing: border-box;
+   box-shadow:0px 0px 0px 3px #9B15B8 inset;
+ }
+
+</style>
